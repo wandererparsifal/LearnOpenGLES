df2e938ee42b672dade807d03aa0893d386d90d7
wandererparsifal <wandererparsifal@gmail.com>
2018-11-21 11:22:15 +0800

    初始化 test3

---
+++ test3/src/main/java/com/example/test3/MyRender.java
@@ -0,0 +1,187 @@
+package com.example.test3;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.util.Log;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by yangming on 18-11-19.
+ */
+public class MyRender implements GLSurfaceView.Renderer {
+
+    private String mVertexShaderCode
+            = "attribute vec4 vPosition;\n" +
+            "varying  vec4 vColor;\n" +
+            "attribute vec4 aColor;\n" +
+            "uniform mat4 vMatrix;\n" +
+            " void main() {\n" +
+            "     gl_Position = vMatrix*vPosition;\n" +
+            "     vColor=aColor;\n" +
+            " }";
+
+    private String mFragmentShaderCode
+            = "precision mediump float;\n" +
+            " varying vec4 vColor;\n" +
+            " void main() {\n" +
+            "     gl_FragColor = vColor;\n" +
+            " }";
+
+    float triangleCoords[] = {
+            0.5f, 0.5f, 0.0f, // top
+            0.0f, 0.0f, 0.0f, // bottom left
+            0.5f, 0.0f, 0.0f, // bottom right
+    };
+
+    //设置颜色
+    float color[] = {
+            0.0f, 1.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+    };
+
+    private FloatBuffer mVertexBuffer;
+
+    private FloatBuffer mColorBuffer;
+
+    private int mProgram;
+
+    private int mPositionHandle;
+
+    private int mColorHandle;
+
+    private int mMatrixHandler;
+
+    private float[] mMVPMatrix = new float[16];
+
+    /**
+     * 加载制定shader的方法
+     *
+     * @param shaderType shader的类型  GLES20.GL_VERTEX_SHADER   GLES20.GL_FRAGMENT_SHADER
+     * @param sourceCode shader的脚本
+     * @return shader索引
+     */
+    private int loadShader(int shaderType, String sourceCode) {
+        // 创建一个新shader
+        int shader = GLES20.glCreateShader(shaderType);
+        // 若创建成功则加载shader
+        if (shader != 0) {
+            // 加载shader的源代码
+            GLES20.glShaderSource(shader, sourceCode);
+            // 编译shader
+            GLES20.glCompileShader(shader);
+            // 存放编译成功shader数量的数组
+            int[] compiled = new int[1];
+            // 获取Shader的编译情况
+            GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
+            if (compiled[0] == 0) {//若编译失败则显示错误日志并删除此shader
+                Log.e("ES20_ERROR", "Could not compile shader " + shaderType + ":");
+                Log.e("ES20_ERROR", GLES20.glGetShaderInfoLog(shader));
+                GLES20.glDeleteShader(shader);
+                shader = 0;
+            }
+        }
+        return shader;
+    }
+
+    private void createProgram() {
+        //将背景设置为灰色
+        GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        //申请底层空间
+        ByteBuffer bb = ByteBuffer.allocateDirect(triangleCoords.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        //将坐标数据转换为FloatBuffer，用以传入给OpenGL ES程序
+        mVertexBuffer = bb.asFloatBuffer();
+        mVertexBuffer.put(triangleCoords);
+        mVertexBuffer.position(0);
+
+        ByteBuffer dd = ByteBuffer.allocateDirect(color.length * 4);
+        dd.order(ByteOrder.nativeOrder());
+        mColorBuffer = dd.asFloatBuffer();
+        mColorBuffer.put(color);
+        mColorBuffer.position(0); //获取片元着色器的vColor成员的句柄
+
+        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, mVertexShaderCode);
+        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, mFragmentShaderCode);
+
+        //创建一个空的OpenGLES程序
+        mProgram = GLES20.glCreateProgram();
+        //将顶点着色器加入到程序
+        GLES20.glAttachShader(mProgram, vertexShader);
+        //将片元着色器加入到程序中
+        GLES20.glAttachShader(mProgram, fragmentShader);
+        //连接到着色器程序
+        GLES20.glLinkProgram(mProgram);
+
+        //获取顶点着色器的vPosition成员句柄
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        //获取片元着色器的vColor成员的句柄
+        mColorHandle = GLES20.glGetAttribLocation(mProgram, "aColor");
+
+        mMatrixHandler = GLES20.glGetUniformLocation(mProgram, "vMatrix");
+    }
+
+    private void draw() {
+        //将程序加入到OpenGLES2.0环境
+        GLES20.glUseProgram(mProgram);
+
+        //指定vMatrix的值
+        GLES20.glUniformMatrix4fv(mMatrixHandler, 1, false, mMVPMatrix, 0);
+
+        //启用三角形顶点的句柄
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+        //准备三角形的坐标数据
+        GLES20.glVertexAttribPointer(mPositionHandle, 3,
+                GLES20.GL_FLOAT, false,
+                0, mVertexBuffer);
+
+        GLES20.glEnableVertexAttribArray(mColorHandle);
+        //设置绘制三角形的颜色
+        GLES20.glVertexAttribPointer(mColorHandle, 4,
+                GLES20.GL_FLOAT, false,
+                0, mColorBuffer);
+        //绘制三角形
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+        //禁止顶点数组的句柄
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+        GLES20.glDisableVertexAttribArray(mColorHandle);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        createProgram();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+
+        float[] mProjectMatrix = new float[16];
+        float[] mViewMatrix = new float[16];
+
+        //计算宽高比
+        float ratio = (float) width / height;
+        //设置透视投影 https://www.aliyun.com/jiaocheng/48374.html
+        Matrix.frustumM(mProjectMatrix, 0,
+                -ratio, ratio, -1, 1, 3, 7);
+        //设置相机位置 https://blog.csdn.net/kkae8643150/article/details/52805738 Matrix.setLookAtM解析
+        Matrix.setLookAtM(mViewMatrix, 0,
+                0, 0, 7.0f,
+                0f, 0f, 0f,
+                0f, 1.0f, 0.0f);
+        //计算变换矩阵
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectMatrix, 0, mViewMatrix, 0);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        draw();
+    }
+}
