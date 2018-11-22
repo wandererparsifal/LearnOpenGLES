034e19050c28ca2c7c01cc69e70e4edabae53a18
wandererparsifal <wandererparsifal@gmail.com>
2018-11-22 14:56:08 +0800

    初始化 test4

---
+++ test4/src/main/java/com/example/test4/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.test4;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView mGlv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGlv = findViewById(R.id.glv);
+        mGlv.setEGLContextClientVersion(2);
+        mGlv.setRenderer(new MyRender());
+    }
+}
