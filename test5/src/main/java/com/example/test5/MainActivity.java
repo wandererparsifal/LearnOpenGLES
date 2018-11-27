decf4c8dfaa7cdfa00d634785400768c5848cb84
wandererparsifal <wandererparsifal@gmail.com>
2018-11-27 14:25:56 +0800

    初始化 test5 绘制纹理

--- test5/src/main/java/com/example/test5/MainActivity.java
+++ test5/src/main/java/com/example/test5/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.test5;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.BitmapFactory;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView mGlv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGlv = findViewById(R.id.glv);
+        mGlv.setEGLContextClientVersion(2);
+
+        String vertex = getFromRaw(R.raw.vertex);
+        String fragment = getFromRaw(R.raw.fragment);
+        mGlv.setRenderer(new MyRender(vertex, fragment,
+                BitmapFactory.decodeResource(getResources(), R.drawable.s)));
+        mGlv.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+    public String getFromRaw(int id) {
+        String result = "";
+        try {
+            InputStream in = getResources().openRawResource(id);
+            int lenght = in.available();
+            byte[] buffer = new byte[lenght];
+            in.read(buffer);
+            result = new String(buffer, "utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
---
+++ test5/src/main/java/com/example/test5/MyRender.java
@@ -0,0 +1,234 @@
+package com.example.test5;
+
+import android.graphics.Bitmap;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
+import android.opengl.Matrix;
+import android.util.Log;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by yangming on 18-11-27.
+ */
+public class MyRender implements GLSurfaceView.Renderer {
+
+    private String mVertexShaderCode;
+
+    private String mFragmentShaderCode;
+
+    private FloatBuffer mVertexBuffer;
+
+    private FloatBuffer mCoordBuffer;
+
+    private int mProgram;
+
+    private Bitmap mBitmap;
+
+    private int mPositionHandler;
+
+    private int mTextureHandler;
+
+    private int mMatrixHandler;
+
+    private float[] mMVPMatrix = new float[16];
+
+    private final float[] sPos = {
+            -1.0f, 1.0f,    //左上角
+            -1.0f, -1.0f,   //左下角
+            1.0f, 1.0f,     //右上角
+            1.0f, -1.0f     //右下角
+    };
+
+    private final float[] sCoord = {
+            0.0f, 0.0f,
+            0.0f, 1.0f,
+            1.0f, 0.0f,
+            1.0f, 1.0f,
+    };
+
+    public MyRender(String v, String f, Bitmap bitmap) {
+        this.mVertexShaderCode = v;
+        this.mFragmentShaderCode = f;
+        this.mBitmap = bitmap;
+    }
+
+    /**
+     * 加载制定shader的方法
+     *
+     * @param shaderType shader的类型  GLES20.GL_VERTEX_SHADER   GLES20.GL_FRAGMENT_SHADER
+     * @param sourceCode shader的脚本
+     * @return shader索引
+     */
+    private int loadShader(int shaderType, String sourceCode) {
+        // 创建一个新shader
+        int shader = GLES20.glCreateShader(shaderType);
+        // 若创建成功则加载shader
+        if (shader != 0) {
+            // 加载shader的源代码
+            GLES20.glShaderSource(shader, sourceCode);
+            // 编译shader
+            GLES20.glCompileShader(shader);
+            // 存放编译成功shader数量的数组
+            int[] compiled = new int[1];
+            // 获取Shader的编译情况
+            GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
+            if (compiled[0] == 0) {//若编译失败则显示错误日志并删除此shader
+                Log.e("ES20_ERROR", "Could not compile shader " + shaderType + ":");
+                Log.e("ES20_ERROR", GLES20.glGetShaderInfoLog(shader));
+                GLES20.glDeleteShader(shader);
+                shader = 0;
+            }
+        }
+        return shader;
+    }
+
+    private int createTexture(Bitmap bitmap) {
+        int[] texture = new int[1];
+        if (bitmap != null && !bitmap.isRecycled()) {
+            //生成纹理
+            GLES20.glGenTextures(1, texture, 0);
+            //生成纹理
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texture[0]);
+            //设置缩小过滤为使用纹理中坐标最接近的一个像素的颜色作为需要绘制的像素颜色
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+            //设置放大过滤为使用纹理中坐标最接近的若干个颜色，通过加权平均算法得到需要绘制的像素颜色
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+            //设置环绕方向S，截取纹理坐标到[1/2n,1-1/2n]。将导致永远不会与border融合
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+            //设置环绕方向T，截取纹理坐标到[1/2n,1-1/2n]。将导致永远不会与border融合
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+            //根据以上指定的参数，生成一个2D纹理
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+            return texture[0];
+        }
+        return 0;
+    }
+
+    private void createProgram() {
+        //将背景设置为灰色
+        GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        //申请底层空间
+        ByteBuffer bb = ByteBuffer.allocateDirect(sPos.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        //将坐标数据转换为FloatBuffer，用以传入给OpenGL ES程序
+        mVertexBuffer = bb.asFloatBuffer();
+        mVertexBuffer.put(sPos);
+        mVertexBuffer.position(0);
+
+        ByteBuffer dd = ByteBuffer.allocateDirect(sCoord.length * 4);
+        dd.order(ByteOrder.nativeOrder());
+        mCoordBuffer = dd.asFloatBuffer();
+        mCoordBuffer.put(sCoord);
+        mCoordBuffer.position(0); //获取片元着色器的vColor成员的句柄
+
+        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, mVertexShaderCode);
+        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, mFragmentShaderCode);
+
+        //创建一个空的OpenGLES程序
+        mProgram = GLES20.glCreateProgram();
+        //将顶点着色器加入到程序
+        GLES20.glAttachShader(mProgram, vertexShader);
+        //将片元着色器加入到程序中
+        GLES20.glAttachShader(mProgram, fragmentShader);
+        //连接到着色器程序
+        GLES20.glLinkProgram(mProgram);
+
+        GLES20.glDetachShader(mProgram, vertexShader);
+        GLES20.glDetachShader(mProgram, fragmentShader);
+        GLES20.glDeleteShader(vertexShader);
+        GLES20.glDeleteShader(fragmentShader);
+
+        //获取顶点着色器的vPosition成员句柄
+        mPositionHandler = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+        mMatrixHandler = GLES20.glGetUniformLocation(mProgram, "vMatrix");
+
+        mTextureHandler = GLES20.glGetAttribLocation(mProgram, "vCoordinate");
+    }
+
+    private void draw() {
+        //将程序加入到OpenGLES2.0环境
+        GLES20.glUseProgram(mProgram);
+
+        //指定vMatrix的值
+        GLES20.glUniformMatrix4fv(mMatrixHandler, 1, false, mMVPMatrix, 0);
+
+        //启用三角形顶点的句柄
+        GLES20.glEnableVertexAttribArray(mPositionHandler);
+        GLES20.glEnableVertexAttribArray(mTextureHandler);
+
+        GLES20.glUniform1i(mTextureHandler, 0);
+
+        //传入顶点坐标
+        GLES20.glVertexAttribPointer(mPositionHandler, 2,
+                GLES20.GL_FLOAT, false,
+                0, mVertexBuffer);
+        //传入纹理坐标
+        GLES20.glVertexAttribPointer(mTextureHandler, 2,
+                GLES20.GL_FLOAT, false,
+                0, mCoordBuffer);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+
+        //禁止顶点数组的句柄
+        GLES20.glDisableVertexAttribArray(mPositionHandler);
+        GLES20.glDisableVertexAttribArray(mTextureHandler);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        createProgram();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+
+        float[] mProjectMatrix = new float[16];
+        float[] mViewMatrix = new float[16];
+
+        int w = mBitmap.getWidth();
+        int h = mBitmap.getHeight();
+        float sWH = w / (float) h;
+        float sWidthHeight = width / (float) height;
+        if (width > height) {
+            if (sWH > sWidthHeight) {
+                Matrix.orthoM(mProjectMatrix, 0,
+                        -sWidthHeight * sWH, sWidthHeight * sWH, -1, 1, 3, 7);
+            } else {
+                Matrix.orthoM(mProjectMatrix, 0,
+                        -sWidthHeight / sWH, sWidthHeight / sWH, -1, 1, 3, 7);
+            }
+        } else {
+            if (sWH > sWidthHeight) {
+                Matrix.orthoM(mProjectMatrix, 0,
+                        -1, 1, -1 / sWidthHeight * sWH, 1 / sWidthHeight * sWH, 3, 7);
+            } else {
+                Matrix.orthoM(mProjectMatrix, 0,
+                        -1, 1, -sWH / sWidthHeight, sWH / sWidthHeight, 3, 7);
+            }
+        }
+        //设置相机位置 https://blog.csdn.net/kkae8643150/article/details/52805738 Matrix.setLookAtM解析
+        Matrix.setLookAtM(mViewMatrix, 0,
+                0, 0, 7f,
+                0f, 0f, 0f,
+                0f, 1.0f, 0.0f);
+        //计算变换矩阵
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectMatrix, 0, mViewMatrix, 0);
+
+        createTexture(mBitmap);
+        mBitmap.recycle();
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        draw();
+    }
+}
