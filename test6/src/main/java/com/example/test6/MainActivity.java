ee1e7502889be87e4257726c27e2e635b44e365f
wandererparsifal <wandererparsifal@gmail.com>
2018-11-28 15:12:05 +0800

    初始化相机预览

--- test6/src/main/java/com/example/test6/MainActivity.java
+++ test6/src/main/java/com/example/test6/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.test6;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Camera mCamera;
+
+    SurfaceHolder mSurfaceHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SurfaceView surfaceView = findViewById(R.id.sv);
+        mSurfaceHolder = surfaceView.getHolder();
+        mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                mCamera = Camera.open(0);
+                try {
+                    mCamera.setPreviewDisplay(holder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mCamera.setDisplayOrientation(90);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                Camera.Parameters parameters = mCamera.getParameters();
+                parameters.setFocusMode("auto");
+                mCamera.setParameters(parameters);
+                mCamera.startPreview();
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mCamera.release();
+                mCamera = null;
+            }
+        });
     }
 }
