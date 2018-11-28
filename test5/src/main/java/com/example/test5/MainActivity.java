36b95b9217c81e87a8be86e4240a50b3b04f7f3e
wandererparsifal <wandererparsifal@gmail.com>
2018-11-28 10:36:19 +0800

    绘制Bitmap

--- test5/src/main/java/com/example/test5/MainActivity.java
+++ test5/src/main/java/com/example/test5/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.test5;
 
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private GLSurfaceView mGlv;
+
+    private SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +29,22 @@ public class MainActivity extends AppCompatActivity {
 
         String vertex = getFromRaw(R.raw.vertex);
         String fragment = getFromRaw(R.raw.fragment);
+
         mGlv.setRenderer(new MyRender(vertex, fragment,
                 BitmapFactory.decodeResource(getResources(), R.drawable.s),
-                BitmapFactory.decodeResource(getResources(), R.drawable.n)));
+                getBitmap(Color.RED)));
         mGlv.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+    public Bitmap getBitmap(int color) {
+        Paint p = new Paint();
+        p.setColor(color);
+        p.setTextSize(50);
+        Bitmap bitmap = Bitmap.createBitmap(800, 600, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        String time = mDateFormat.format(Calendar.getInstance().getTime());
+        canvas.drawText(time, 100, 100, p);
+        return bitmap;
     }
 
     public String getFromRaw(int id) {
--- test5/src/main/res/raw/fragment.glsl
+++ test5/src/main/res/raw/fragment.glsl
@@ -9,5 +9,10 @@ varying vec2 aCoordinate2;
 void main(){
     vec4 texture1 = texture2D(vTexture, aCoordinate);
     vec4 texture2 = texture2D(vTexture2, aCoordinate2);
-    gl_FragColor=texture1+texture2;
+    vec4 vv = vec4(0.0,0.0,0.0,0.0);
+    if (texture2 == vv) {
+        gl_FragColor=texture1;
+    } else {
+        gl_FragColor=texture2;
+    }
 }
